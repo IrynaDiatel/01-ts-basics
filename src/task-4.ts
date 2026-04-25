6f51abda27900948925af9a2a659b3a5524d4465
IrynaDiatel <kastyrinaira@gmail.com>
2026-04-25 12:54:44 +0300

    solving task 4

--- src/task-4.ts
+++ src/task-4.ts
@@ -12,8 +12,21 @@ const products: Product[] = [
   { id: 2, title: "Laptop" },
 ];
 
+
+function printUserInfo(name: string, age: number, email?: string): void {
+  console.log(`Name: ${name}`);
+  console.log(`Age: ${age}`);
+  if (email) {
+    console.log(`Email: ${email}`);
+  }
+}
+
+
+printUserInfo("Alice", 25, "alice@example.com");
+printUserInfo("Bob", 30);
+
 console.log(`Usernames: ${JSON.stringify(usernames)}`);
 console.log(`Ratings: ${JSON.stringify(ratings)}`);
-console.log(`Products: ${JSON.stringify(products)}`); 
+console.log(`Products: ${JSON.stringify(products)}`);
 
 export {};
